405a339d431973c322672c173e5be51c9eba4780
Erin Wulber <erin.wulber@urbansdk.com>
2021-04-11 15:42:20 -0400

    added role name mapping

--- src/sakuno.js
+++ src/sakuno.js
@@ -41,25 +41,51 @@ client.on('message', async (message) => {
     let char_info = undefined;
     
     switch (command) {
-      case 'sakuno-gms': 
+      case 'sakuno-gms':
         char_info = await verify_hayato(args[0], 'gms');
-        if (char_info) add_role(message, char_info?.server);
+        if (char_info) {
+          add_role(message, char_info?.server);
+          assign_name(message, args[0]);
+        } else {
+          message.channel.send('You must enter the ign of your hayato to enter. Make sure that it is spelt correctly!');
+        }
         break;
       case 'sakuno-ems':
         char_info = await verify_hayato(args[0], 'ems');
-        if (char_info) add_role(message, char_info?.server);
+        if (char_info) {
+          add_role(message, char_info?.server);
+          assign_name(message, args[0]);
+        } else {
+          message.channel.send('You must enter the ign of your hayato to enter. Make sure that it is spelt correctly!');
+        }
         break;
       case 'sakuno-ps':
-        add_role(message, 'Private Server');
+        add_role(message, 'ps');
+        assign_name(message, args[0]);
         break;
       case 'sakuno-cms':
-        add_role(message, 'CMS');
+        add_role(message, 'cms');
+        assign_name(message, args[0]);
         break;
       case 'sakuno-kms':
-        add_role(message, 'KMS');
+        add_role(message, 'kms');
+        assign_name(message, args[0]);
         break;
-      case 'sakuno-tms':
-        add_role(message, 'TMS');
+      case 'sakuno-msea':
+        add_role(message, 'msea');
+        assign_name(message, args[0]);
+        break;
+      case 'help':
+        message.channel.send(`\`\`\`
+        available commands:
+        - sakuno-gms: 1 argument, [ign] This command gives the user a server role and assigns the user's nickname to the provided ign. /sakuno-gms hamtoryu\n
+        - sakuno-ems: 1 argument, [ign] This command gives the user a server role and assigns the user's nickname to the provided ign. /sakuno-ems hamtoryuwu\n
+        - sakuno-ps: 1 argument, [ign] This command gives the user a server role and assigns the user's nickname to the provided ign. /sakuno-ps hamtoryowo\n
+        - sakuno-cms: 1 argument, [ign] This command gives the user a server role and assigns the user's nickname to the provided ign. /sakuno-cms hamtoworyu\n
+        - sakuno-kms: 1 argument, [ign] This command gives the user a server role and assigns the user's nickname to the provided ign. /sakuno-kms hamtoworyuwu\n
+        - sakuno-msea: 1 argument, [ign] This command gives the user a server role and assigns the user's nickname to the provided ign. /sakuno-msea hamtoworyowo\n
+        - help: this? O_O
+        \`\`\``);
         break;
       default:
         message.channel.send('invalid command ;-;');
@@ -78,7 +104,7 @@ const verify_hayato = async (ign, server) => {
     };
     return relevant_info.job === 'Hayato' && relevant_info;
   } catch (err) {
-    console.log(error);
+    console.log(err);
     return false;
   }
 }
@@ -90,14 +116,43 @@ const is_valid_command = (
   return typeof command === 'string' && Array.isArray(args);
 };
 
-// TODO: add map to translate from gg server id to role name
-const add_role = (msg, role_name) => {
-  const role = msg.guild.roles.cache.find(r => r.name.toLowerCase() === role_name);
-  const member = msg?.guild?.members?.cache.get(msg?.author?.id);
-  member.roles.add(role).then((res) => console.log(res?.data)).catch(console.error);
+const roles_map = {
+  'Reboot (NA)': 'Reboot',
+  'Reboot (EU)': 'Reboot EU',
+  'Scania': 'Scania',
+  'Bera': 'Bera',
+  'Aurora': 'Aurora',
+  'Elysium': 'Elysium',
+  'Luna': 'Luna',
+  'ps': 'Private Server',
+  'msea': 'MapleSEA',
+  'cms': 'CMS',
+  'kms': 'KMS',
 };
 
-// TODO: everything... :)
-const assign_name = (msg, ign) => {};
+// TODO: add map to translate from gg server id to role name
+const add_role = (msg, key) => {
+  const role = msg.guild.roles.cache.find(r => r.name === roles_map[key]);
+  const member = msg?.guild?.members?.cache.get(msg?.author?.id);
 
-verify_hayato('beanary', 'gms');
+  if (msg.member.roles.cache.some(r=>Object.keys(roles_map).map(key => roles_map[key]).includes(r.name))) {
+    msg.channel.send('You have already been assigned a role!');
+    return;
+  };
+
+  member.roles.add(role).then((res) => console.log(res?.data)).catch((error) => {
+    channel.send('An error occurred while adding your role! Please try again :3');
+    console.log(error);
+  });
+};
+
+const assign_name = (msg, ign) => {
+  const member = msg?.guild?.members?.cache.get(msg?.author?.id);
+
+  if (msg.member.roles.cache.some(r=>Object.keys(roles_map).map(key => roles_map[key]).includes(r.name))) return;
+
+  member.setNickname(ign).catch((error) => {
+    msg.channel.send('An error occurred while setting your nickname! Please try again :3');
+    console.log(error);
+  });
+};
